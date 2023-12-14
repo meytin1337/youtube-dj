37ec957e135f29e50e57253252e89235019bc04e
Mats Heemeyer <meytin@protonmail.com>
2023-12-14 19:40:00 +0100

    Include frontend domain in cors rules

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,12 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+CORS(
+    app,
+    resources={
+        r"/*": {"origins": ["http://localhost:3000", "https://ydj.heemeyer.net"]}
+    },
+)
 
 import yt_dlp
 
